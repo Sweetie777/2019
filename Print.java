838d6ebe247624be94796a3876101b78e15f32b8
Sweetie77 <1443354525@qq.com>
2019-03-07 22:21:17 +0800

    提交代码

---
+++ Print.java
@@ -0,0 +1,23 @@
+package com.bittech.Practice;
+
+/**
+ * Author: Sweetie77
+ * Created: 2019/3/5
+ */
+public class Print {
+    /**
+     * 不带回车
+     * @param s
+     */
+    public static void print(Object s){
+        System.out.print(s);
+    }
+
+    /**
+     * 带回车
+     * @param s
+     */
+    public static void println(Object s){
+        System.out.println(s);
+    }
+}
---
+++ Velocity.java
@@ -0,0 +1,15 @@
+package com.bittech.Practice;
+
+/**
+ * Author: Sweetie77
+ * Created: 2019/3/5
+ */
+public class Velocity {
+    public static void main(String[] args) {
+        float time = 100;
+        float distance = 20;
+        float velocity = distance / time;
+
+        System.out.println("速度是: " + velocity + "m/s");
+    }
+}
