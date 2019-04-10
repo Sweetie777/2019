97292e1bbebea07a3d7808a713c8db048e7a8fc4
Sweetie77 <1443354525@qq.com>
2019-04-10 19:12:10 +0800

    提交代码

--- Nowcoder1.java
+++ Nowcoder1.java
@@ -6,7 +6,7 @@ import java.util.List;
  * Author: Sweetie77
  * Created: 2019/4/9
  */
-public class Nowcoder {
+public class Nowcoder1 {
 
     public class ListNode {
         int val;
@@ -54,8 +54,56 @@ public class Nowcoder {
             }
         }
 
-        public static void main(String[] args) {
+        public int length(ListNode head){
+            int len = 0;
+            ListNode cur= head;
+            while(cur != null){
+                len++;
+                cur = cur.next;
+            }
+            return len;
+        }
 
+        public ListNode reverse(ListNode head){
+            ListNode result = null;
+            ListNode cur = head;
+            while(cur != null){
+                ListNode next = cur.next;
+                cur.next = result;
+                result = cur;
+                cur = next;
+            }
+            return result;
+        }
+
+        public boolean chkPalindrome(ListNode A) {
+            // write code here
+            int len = length(A);
+            int halfLen = len / 2;
+
+            ListNode middle = A;
+            for (int i = 0; i < halfLen; i++) {
+                middle = middle.next;
+            }
+
+            ListNode r = reverse(middle);
+            ListNode c1 = A;
+            ListNode c2 = r;
+
+
+            while (c1 != null && c2 != null) {
+                if (c1.val != c2.val) {
+                    return false;
+                }
+
+                c1 = c1.next;
+                c2 = c2.next;
+            }
+
+            return true;
+        }
+
+        public static void main(String[] args) {
 
         }
     }
