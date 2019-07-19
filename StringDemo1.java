8af5720e27df434d2e125b1153ac86769057ecaa
Sweetie77 <1443354525@qq.com>
2019-07-19 22:14:41 +0800

    StringDemo1

---
+++ StringDemo1.java
@@ -0,0 +1,20 @@
+package base;
+
+/**
+ * Author: Sweetie77
+ * Created: 2019/7/16
+ */
+public class StringDemo1 {
+    public static void main(String[] args) {
+        String str1 = new String("abc");//2
+        String str2 = new String("abc");//1
+        String str3 = new String("abcd");//2
+    }
+
+
+    /**
+     * 开辟了几个空间
+     * A.3  B.4  C.5  D.6
+     *
+     */
+}
