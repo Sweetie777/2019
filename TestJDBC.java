44196536d1794d471973713cdd6550649896de18
Sweetie77 <1443354525@qq.com>
2019-08-22 22:51:25 +0800

    testJDBC

--- TestJDBC.java
+++ TestJDBC.java
@@ -1,84 +1,65 @@
-package com.bittech;
+package JDBC;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.time.LocalDateTime;
+import java.sql.*;
 
+/**
+ * Author: Sweetie77
+ * Created: 2019/8/22
+ */
+public class testJDBC {
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet resultSet = null;
 
-public class TestJDBC {
-
-    public static void main(String[] args) {
-        //1.加载驱动程序
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+    try{
+        /**
+         * 加载驱动有两种方式
+         * 1. 会导致驱动会注册两次,过度依赖mysql的api,脱离mysql的开发包,程序将无法编译
+         * 2. 驱动只会加载一次,不需要依赖具体的驱动,灵活性高
+         */
+        //1.
+        // DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        //2.
+        Class.forName("com.mysql.jdbc.Driver");
+        //获取与数据库连接的对象Connection
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/xxx", "root", "root");
+        //获取执行sql语句的statement对象
+        statement = connection.createStatement();
+        //执行sql语句,拿到结果集
+        resultSet = statement.executeQuery("SELECT * FROM user");
+        //遍历结果集,得到数据
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString(1));
+            System.out.println(resultSet.getString(2));
         }
-
-        //2.获取连接
-        //JDBC API
-        //第一种：DriverManager
-        //第二种：DataSource
-        //url :   JDBC url schema :
-        //       jdbc:<数据库产品名称>://host:port/<databaseName>?param1=value1&param2=value2
-        //user : root
-        //password : root
-        String url = "jdbc:mysql://127.0.0.1:3306/memo";
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
-        try {
-            connection = DriverManager.getConnection(url, "root", "root");
-
-            //3. 创建命令
-            statement = connection.createStatement();
-
-            //4. 准备SQL语句，执行
-            String sql = "select id ,name,created_time, modify_time  from memo_group";
-            resultSet = statement.executeQuery(sql);
-
-            //5.返回结果，处理结果
-            while (resultSet.next()) {
-                //按行取，每一行都有很多列
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                LocalDateTime createdTime = resultSet.getTimestamp("created_time")
-                        .toLocalDateTime();
-                LocalDateTime modifyTime = resultSet.getTimestamp("modify_time").toLocalDateTime();
-                System.out.println(String.format(
-                        "编号：%d   名称：%s   创建时间：%s  修改时间：%s"
-                        , id, name, createdTime, modifyTime));
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }finally {
-            //6.关闭资源
-            //结果 -> 命令 -> 连接
-            if(resultSet!=null){
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if(statement !=null){
-                try {
-                    statement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if(connection!= null){
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+    }catch(SQLException e){
+        e.printStackTrace();
+    }catch(ClassNotFoundException e){
+        e.printStackTrace();
+    }finally{
+        //关闭资源,后调的先关闭
+        //关闭之前,要先判断对象是否存在
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
+        if (statement != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (connection != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
