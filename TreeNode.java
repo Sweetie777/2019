cf321a89a230998ada05d5742258b77609b4819c
Sweetie77 <1443354525@qq.com>
2019-08-24 23:15:32 +0800

    TreeNode

---
+++ TreeNode.java
@@ -0,0 +1,15 @@
+package tree;
+
+import sun.reflect.generics.tree.Tree;
+
+/**
+ * Author: Sweetie77
+ * Created: 2019/8/23
+ */
+public class TreeNode {
+    int  val;
+    TreeNode left;
+    TreeNode right;
+
+
+}
